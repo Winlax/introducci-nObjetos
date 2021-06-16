f86a016528dc128048f5ae7d0c222717529e45e0
William Castillo <minewillicraft@gmail.com>
2021-06-15 23:21:01 -0600

    Cree repositorio y añadi Ejercicio 1 Contador

---
+++ Contador.java
@@ -0,0 +1,40 @@
+public class Contador {
+    public static void main(String[] args) {
+        
+       Contador  a = new Contador();
+       
+       a.setNumeros(10);
+       System.out.println("Numero de contador ingresado: "+ a.getNumeros());
+       System.out.println("Contador decreciente: "+ a.decrementarContador(a.getNumeros()));
+       System.out.println("Contador incrementado: "+ a.incrementarContador(a.getNumeros()));
+       
+    }
+    
+    private int numeros;
+
+    public Contador() {
+        
+    }
+
+    public int getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(int numeros) {
+        this.numeros = numeros;
+    }
+    
+    
+    
+    public int incrementarContador(int numero) {
+        numero += 1;
+        return numero;
+    }
+    
+    public int decrementarContador(int numero) {
+        numero -= 1;
+        return numero;
+    }
+    
+    
+}
