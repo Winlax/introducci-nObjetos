0e73d2ea4fdb4ca56e7645b28c41ab12659d1d8a
William Castillo <minewillicraft@gmail.com>
2021-06-16 00:49:20 -0600

    Añadi Ejercicio 6 Libro

---
+++ Libro.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Libro {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        Libro a = new Libro();
+        String nombre = "Divina Comedia";
+
+        a.setNombreLibro(nombre);
+        
+        a.Prestamo(nombre);
+        a.Devolucion(nombre);
+        a.infoLibro(nombre);
+
+    }
+
+    private String nombreLibro;
+
+    public Libro() {
+    }
+
+    public Libro(String nombreLibro) {
+        this.nombreLibro = nombreLibro;
+    }
+
+    public String getNombreLibro() {
+        return nombreLibro;
+    }
+
+    public void setNombreLibro(String nombreLibro) {
+        this.nombreLibro = nombreLibro;
+    }
+
+    public void Prestamo(String nombre) {
+        System.out.println("Usted a prestado el libro " + nombre);
+    }
+
+    public void Devolucion(String nombre) {
+        System.out.println("Usted a devuelto el libro " + nombre);
+    }
+
+    public void infoLibro(String nombre) {
+        System.out.println(nombre + ": trata sobre una persona llamada Dante que busca...");
+    }
+}
