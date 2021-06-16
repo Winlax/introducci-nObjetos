8bf7e6488126c254c628c01428d2e2f2da18c655
William Castillo <minewillicraft@gmail.com>
2021-06-16 00:48:33 -0600

    Añadi Ejercicio 5 Contador2

---
+++ Contador2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class Contador2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int contador;
+        
+        System.out.println("Ingrese un contador:");
+        contador = teclado.nextInt();
+        
+        Contador2 a = new Contador2(contador);
+        a.setContador(contador);
+       System.out.println("Numero de contador ingresado: "+ a.getContador());
+       System.out.println("Contador decreciente: "+ a.decrementarContador(a.getContador()));
+       System.out.println("Contador incrementado: "+ a.incrementarContador(a.getContador()));
+       
+    }
+    
+    private int contador;
+    
+    public Contador2() {
+        
+    }
+
+    public Contador2(int contador) {
+        this.contador = contador;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+    
+    public int incrementarContador(int numero) {
+        numero += 1;
+        return numero;
+    }
+    
+    public int decrementarContador(int numero) {
+        numero -= 1;
+        return numero;
+    }
+}
